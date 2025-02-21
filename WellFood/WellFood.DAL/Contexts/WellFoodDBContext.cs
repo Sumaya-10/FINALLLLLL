ce2f963151687228e57f910a77ce81da529dc13d
Sumaya-10 <sumayam-ab108@code.edu.az>
2025-02-21 23:47:22 +0400

    ayamaaaaan

---
+++ WellFood/WellFood.DAL/Contexts/WellFoodDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WellFood.DAL.Models;
+
+namespace WellFood.DAL.Contexts;
+
+public class WellFoodDBContext:DbContext
+{
+    private readonly string _connectionStr = @"Server=WIN-LIOGS5LQGIQ\SQLEXPRESS;Database=WellFoodDB;Trusted_Connection=True;TrustServerCertificate=True";
+    public DbSet<SliderItem> SliderItems { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_connectionStr);
+        base.OnConfiguring(optionsBuilder);
+    }
+}
--- WellFood/WellFood/obj/Debug/net8.0/WellFood.MVC.AssemblyInfo.cs
+++ WellFood/WellFood/obj/Debug/net8.0/WellFood.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WellFood.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+025f9dc1b98762240ecbb3096a8736b60e41086a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WellFood.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WellFood.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
